0bddae54473bc6277dbd826423cab4aa47a912c6
csachin42 <csachin42@gmail.com>
2022-06-03 20:15:26 +0530

    Added Array List

---
+++ src/Workshop3P/DemoArrayList.java
@@ -0,0 +1,12 @@
+package Workshop3P;
+import java.util.ArrayList;
+
+public class DemoArrayList{
+    public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList<String>();
+        list.add("Sachin");
+        list.add("Manoj");
+        list.add("Ketan");
+        System.out.println(list);
+    }
+}
